0696fe32df7fc906a1c1cf28da515d81cf0a74be
Furkan Cerk <104474662+furkancerk@users.noreply.github.com>
2022-05-01 01:47:43 +0300

    Dönem sonu not hesaplama eklendi.

---
+++ DonemSonuNotHesaplama.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonemSonuNotuHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v, f, ort;
+            Console.WriteLine("Dönem Sonu Notu Hesaplama!");
+            Console.WriteLine();
+
+            Console.Write("Vize: ");
+            v = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Final: ");
+            f = Convert.ToInt16(Console.ReadLine());
+
+            ort = (v * 40 / 100) + (f * 60 / 100);
+
+            Console.WriteLine("Ortalama: " + ort);
+
+            Console.Read();
+
+                
+
+
+
+
+        }
+    }
+}
